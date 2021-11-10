e06865de056589dc24cc9aa67a1690964cbd2c2b
Fvaldenegro95 <fvaldenegro95@gmail.com>
2021-11-10 17:40:34 -0500

    felipe-valdenegro

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import MovieHeader from './components/MovieHeader';
 
 import EditMovieForm from './components/EditMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
+import AddMovieForm from './components/AddMovieForm'
 
 import axios from 'axios';
 
@@ -45,10 +46,16 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies}/>
             </Route>
 
+            <Route path="/movies/add/" component={AddMovieForm}>
+              <AddMovieForm setMovies={ setMovies}/>
+            </Route>
+
+
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie}/>
             </Route>
 
             <Route path="/movies">
